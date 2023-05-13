7ccc19539337b3470b2447b8b0514fcf9a509196
bonte <bonte.lucas234@gmail.com>
2023-05-13 21:52:31 +0300

    Adaugat exercitii.py

---
+++ exercitii.py
@@ -0,0 +1,50 @@
+# sa se defineasca o functie care primeste ca parametru un numar natural si o lista de numere naturale si retoarne true daca numarul natural aflat in parametru este gasit in lista si false in caz contrar
+def functie(lista, n):
+    for i in range(len(lista)):
+        if n == lista[i]:
+            return true
+    return false
+    lista = [1, 2, 3]
+    n = 2
+
+# definiti o functie care primeste ca parametru o matrice de numere naturale si un numar natural si functia sa retoarne true daca imi gaseste numarul natural si false in caz contrar
+
+
+
+def cautarem(n,matrice):
+    r = len(matrice)
+    c = len(matrice[0])
+    for i in range(r):
+        for j in range(c):
+            if matrice[i][j] == n
+                return f
+    return f
+
+# sa se scrie o functie recursiva care calculeaza factorialul unui numar natural
+
+
+def factorial(n):
+    if n==0:
+        return 1
+    return n * factorial(n-1)
+
+def f(n):
+    d = n//10
+    r = n%10
+    return n<f(d)
+
+# sa se scrie o functie care implementeaza algoritmul bubblesort pe o lista
+
+def bubblesort():
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    factorial()
